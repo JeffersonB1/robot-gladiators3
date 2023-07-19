c593abf69305ea91d0b03c0c209377098ea1a410
shiba inu <jefferson121996@gmail.com>
2023-07-19 18:58:15 -0400

    creating variables and assigning name to it, working on examples

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName= window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10+10);
+console.log("Our robot's name is " + playerName);
+console.log("Tony the robot " + " is ready for the battle!");
+console.log(playerName + " is ready for the battle!");
+console.log("Your player " + playerName + " is ready for the battle!");
+
+// this creates a function call fight
+
+function fight() {
+    window.alert("The fight has begun!")
+}
+
+//fight();
